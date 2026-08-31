f57de661b4b1c32c927ec1293081820b0e282c0f
Nova <nayakatirtha25@gmail.com>
2026-08-31 14:41:45 +0700

    search

--- config/back.config.services.php
+++ config/back.config.services.php
@@ -1,9 +1,6 @@
 <?php
 
 return [
-    // API Key YouTube Data API v3, dipakai DiscoverController untuk
-    // mengambil video/musik "otomatis" tanpa perlu di-upload manual.
-    // Daftar gratis di https://console.cloud.google.com
     'youtube' => [
         'key' => env('YOUTUBE_API_KEY'),
     ],
