9a8a94ceac4688654a2c74000b509938d2fa2334
Nova <nayakatirtha25@gmail.com>
2026-09-04 08:54:46 +0700

    musik

--- app/Services/EngagementService.php
+++ app/Services/EngagementService.php
@@ -232,5 +232,16 @@ class EngagementService
             ['target_type' => $type, 'target_key' => $key],
             ['likes' => $likes, 'dislikes' => $dislikes]
         );
-    }
+    }<?php
+
+return [
+    'youtube' => [
+        'key' => env('YOUTUBE_API_KEY'),
+    ],
+
+    'spotify' => [
+        'client_id' => env('SPOTIFY_CLIENT_ID'),
+        'client_secret' => env('SPOTIFY_CLIENT_SECRET'),
+    ],
+];
 }
